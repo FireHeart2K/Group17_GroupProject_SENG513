a64dfbdfc342bdf79000d3c39716bc5120981772
Nathan <nathan.bhandari@ucalgary.ca>
2022-03-31 11:13:11 -0600

    Added tables

--- server/awsdb.js
+++ server/awsdb.js
@@ -4,7 +4,8 @@ var connection = mysql.createConnection({
     host     : "xv-db.cbbcel7ahipi.us-east-1.rds.amazonaws.com",
     user     : "admin",
     password : "3RpePTwPyWsYa60kTP3BNTFP0EpMibbbmrgQ97tb",
-    port     : 3306
+    port     : 3306,
+    database: "exVaultDB"
 });
 
 connection.connect(function(err) {
@@ -14,6 +15,14 @@ connection.connect(function(err) {
     }
 
     console.log('Connected to database.');
+    var sql = "INSERT INTO files (filename, file) VALUES ('Project_Proposal4.pdf', LOAD_FILE('server/Project_Proposal1.pdf'))";
+    connection.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("1 record inserted");
+    });
+    connection.query("SELECT * FROM files", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+      });
+      
 });
-
-connection.end();
