1a78e691fd5e78dfeee5affbd0d87f59ffe5f425
Nathan <nathan.bhandari@ucalgary.ca>
2022-04-03 14:43:33 -0600

    Got file insert/retrieval from awsdb working

--- server/awsdb.js
+++ server/awsdb.js
@@ -1,4 +1,7 @@
 var mysql = require('mysql');
+const fs = require('fs')
+var nb = require('buffer');
+
 
 var connection = mysql.createConnection({
     host     : "xv-db.cbbcel7ahipi.us-east-1.rds.amazonaws.com",
@@ -13,16 +16,25 @@ connection.connect(function(err) {
         console.error('Database connection failed: ' + err.stack);
         return;
     }
-
     console.log('Connected to database.');
-    var sql = "INSERT INTO files (filename, file) VALUES ('Project_Proposal4.pdf', LOAD_FILE('server/Project_Proposal1.pdf'))";
-    connection.query(sql, function (err, result) {
+    fs.readFile('/Users/Nathan/Group17_GroupProject_SENG513/server/Project_Proposal1.pdf', 'base64',(err, data) => {
       if (err) throw err;
-      console.log("1 record inserted");
-    });
-    connection.query("SELECT * FROM files", function (err, result, fields) {
+      //var data1 = new nb.Blob(data);
+
+      /*
+      var sql = "INSERT INTO files (fileID, fileBLOB) VALUES ('Project_Proposal4.pdf', " + "'" + data + "'" + ")";
+      connection.query(sql, function (err, result) {
         if (err) throw err;
-        console.log(result);
+        console.log("1 record inserted");
       });
+      */
+      connection.query("SELECT fileBLOB FROM files WHERE fileID='Project_Proposal4.pdf'", function (err, result, fields) {
+          if (err) throw err;
+          //https://stackabuse.com/encoding-and-decoding-base64-strings-in-node-js/
+          let buff = new Buffer(data, 'base64');
+          fs.writeFileSync('Project_Proposal2.pdf', buff);
+        });
+    });
+
       
 });
