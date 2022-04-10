5e2cd7f66cdb912bbb4225c85f62c7261d433826
Irtaza Ansari <36017101+FireHeart2K@users.noreply.github.com>
2022-04-09 23:39:40 -0400

    Create Server with CSS
    Create Server with CSS

--- ex-vault/src/App.js
+++ ex-vault/src/App.js
@@ -5,11 +5,12 @@ import SignUp from './components/SignUp';
 import DirectoryList from './components/DirectoryList';
 import ActiveRooms from './components/AcriveRooms';
 import './App.css';
+import CreateServer from './components/CreateServer';
 
 function App() {
     return (
         <div className='App'>
-            <ActiveRooms/>
+            <CreateServer/>
         </div>
     )
 
--- ex-vault/src/components/CreateServer.js
+++ ex-vault/src/components/CreateServer.js
@@ -1,23 +1,27 @@
 import {React, Button} from "react";
+import './CreateServer.css';
 
 
 function CreateServer() {
   return (
-    <div>
+    <div className="CreateServerContainer">
+      <div>
       <button className="BackButtonCreateServer">Back</button>
       <div className="createServerinputFields">
         <input
-        className="input"
+        className="CreateServerInputFields"
         type="text"
         placeholder="Server Name..."
         />
         <inpu className="input" type="text" placeholder="Description..."/>
+        <div>
         <input
-          className="input"
+          className="CreateServerInputFields"
           type="text"
           placeholder="Server Password (optional)"
-          />
+          /></div>
         <button className="create-server-button">Creater Server</button>
+        </div>
         </div>
     </div>
   );
