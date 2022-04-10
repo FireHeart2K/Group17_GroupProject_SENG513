ebb54720434f898dd99b09b6890f61b06df2ad1e
Irtaza Ansari <36017101+FireHeart2K@users.noreply.github.com>
2022-04-10 15:23:21 -0400

    User's shared files updated list.
    User's shared files updated list of items to display.

--- ex-vault/src/components/UsersFiles.js
+++ ex-vault/src/components/UsersFiles.js
@@ -25,6 +25,14 @@ class DirectoryList extends Component{
                         <li><img src='https://cdn.discordapp.com/attachments/935458880467632181/962792826360442880/zip_file_icon.png' style={{ width: "20px", height: "20px" }} alt={".."}/> compressed_data.zip</li><li><img src='https://cdn.discordapp.com/attachments/935458880467632181/962790775551311872/PDF-Icon.png' style={{ width: "20px", height: "20px" }} alt={".."}/> PDF File</li>
                         <li><img src='https://media.discordapp.net/attachments/935458880467632181/962794015080722482/JPG_image_icon.png' style={{ width: "20px", height: "20px" }} alt={".."}/> Image 1</li>
                         <li><img src='https://media.discordapp.net/attachments/935458880467632181/962794015080722482/JPG_image_icon.png' style={{ width: "20px", height: "20px" }} alt={".."}/> Image 2</li>
+                        <li><img src='https://cdn.discordapp.com/attachments/935458880467632181/962790775551311872/PDF-Icon.png' style={{ width: "20px", height: "20px" }} alt={".."}/> Notes.pdf</li>
+                        <li><img src='https://cdn.discordapp.com/attachments/935458880467632181/962791667520372846/MP3_File_Icon.png' style={{ width: "20px", height: "20px" }} alt={".."}/> Audio Book.mp3</li>
+                        <li><img src='https://cdn.discordapp.com/attachments/935458880467632181/962792826360442880/zip_file_icon.png' style={{ width: "20px", height: "20px" }} alt={".."}/> compressed_data.zip</li>
+                        <li><img src='https://cdn.discordapp.com/attachments/935458880467632181/962790775551311872/PDF-Icon.png' style={{ width: "20px", height: "20px" }} alt={".."}/> Notes.pdf</li>
+                        <li><img src='https://cdn.discordapp.com/attachments/935458880467632181/962791667520372846/MP3_File_Icon.png' style={{ width: "20px", height: "20px" }} alt={".."}/> Audio Book.mp3</li>
+                        <li><img src='https://cdn.discordapp.com/attachments/935458880467632181/962792826360442880/zip_file_icon.png' style={{ width: "20px", height: "20px" }} alt={".."}/> compressed_data.zip</li><li><img src='https://cdn.discordapp.com/attachments/935458880467632181/962790775551311872/PDF-Icon.png' style={{ width: "20px", height: "20px" }} alt={".."}/> PDF File</li>
+                        <li><img src='https://media.discordapp.net/attachments/935458880467632181/962794015080722482/JPG_image_icon.png' style={{ width: "20px", height: "20px" }} alt={".."}/> Image 1</li>
+                        <li><img src='https://media.discordapp.net/attachments/935458880467632181/962794015080722482/JPG_image_icon.png' style={{ width: "20px", height: "20px" }} alt={".."}/> Image 2</li>
                     </ul>
                 </div>
                 <div id = "row-3">
