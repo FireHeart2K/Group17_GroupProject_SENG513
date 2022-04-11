6ca72e61ee82c8b6056b85d8c20f8d6c9c9a97f1
Nathan <nathan.bhandari@ucalgary.ca>
2022-04-11 15:15:24 -0600

    added js structure and fixed css

--- awsdb.js
+++ awsdb.js
@@ -7,6 +7,9 @@ const bodyParser = require('body-parser');
 const cors = require('cors')
 const fs = require('fs')
 var nb = require('buffer');
+var cookieParser = require('cookie-parser');
+const req = require('express/lib/request');
+const router = express.Router();
 
 
 const db = mysql.createConnection({
@@ -18,20 +21,64 @@ const db = mysql.createConnection({
 });
 
 let app = express();
-let server = app.listen(3000);
-app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+//Cookie code from https://stackoverflow.com/questions/16209145/how-can-i-set-cookie-in-node-js-using-express-framework
+app.use(cookieParser())
+app.use(function (req, res, next) {
+    // check if client sent cookie
+    var cookie = req.cookies.cookieName;
+    if (cookie === undefined) {
+        // no: set a new cookie
+        var randomNumber = Math.random().toString();
+        randomNumber = randomNumber.substring(2, randomNumber.length);
+        res.cookie('cookieName', randomNumber, { maxAge: 900000, httpOnly: true });
+        console.log('cookie created successfully');
+    } else {
+        // yes, cookie was already present 
+        console.log('cookie exists', cookie);
+    }
+    next(); // <-- important!
 });
 
+// let static middleware do its job
+app.use(express.static('public'))
+app.post('/json-handler', (req, res) => {
+    
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/ActiveRooms.html');
+});
+
+app.get('/create', (req, res) => {
+    res.sendFile(__dirname + '/CreateServer.html');
+});
+app.get('/join', (req, res) => {
+    res.sendFile(__dirname + '/EnterServer.html');
+});
+app.get('/chatroom', (req, res) => {
+    res.sendFile(__dirname + '/ChatScreen.html');
+});
+/*
+router.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, '/ActiveRooms.html'));
+    console.log("HERE")
+});
+*/
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/SignUp.html');
+});
+
+let server = app.listen(3000);
 let io = socket(server);
+
 
 db.connect(function (err) {
     if (err) {
         console.error('Database connection failed: ' + err.stack);
         return;
     }
+
     console.log('Connected to database.');
     io.sockets.on('connection', (socket) => {
         console.log('user connected');
@@ -55,11 +102,13 @@ db.connect(function (err) {
     });
     io.sockets.on('connection', (socket) => {
         socket.on('newuser', (username, password) => {
-            var sql = "INSERT INTO users (userID, password) VALUES (" + "'" +username + "'" + ","  + "'" + password + "'" + ")";
+            /*
+            var sql = "INSERT INTO users (userID, password) VALUES (" + "'" + username + "'" + "," + "'" + password + "'" + ")";
             db.query(sql, function (err, result) {
                 if (err) throw err;
                 console.log("1 record inserted");
             });
+            */
         });
     });
 });
--- public/client.js
+++ public/client.js
@@ -1,13 +1,28 @@
+
 /*This file was inspired the socket.io chat tutorial https://socket.io/get-started/chat and the lecture of the joint painting by the professor https://d2l.ucalgary.ca/d2l/le/content/422910/viewContent/5219346/View*/
 let socket = io.connect('http://localhost:3000');
 
-newsignon.addEventListener('submit', function (e) {
+document.getElementById("create-Server-Button").addEventListener("click", function (e) {
   e.preventDefault();
-  if (un.value && pass.value) {
-    socket.emit('newuser', un.value,pass.value)
+  if (document.getElementById("email").value && document.getElementById("password").value && document.getElementById("confirmPassword").value && document.getElementById("password").value == document.getElementById("confirmPassword").value) {
+    var jsonStuff = { "email": document.getElementById("email").value, "password": document.getElementById("password").value }
+    var toSend = JSON.stringify(jsonStuff)
+    const xhttp = new XMLHttpRequest();
+    // Send a request
+    xhttp.open("POST", "/json-handler");
+    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhttp.send(toSend);
+    //change page req
+    const xhttp2 = new XMLHttpRequest();
+
+    xhttp2.open("GET", "/login");
+    //xhttp2.setRequestHeader('Login', 'one');
+    xhttp2.send(toSend);
+    //socket.emit('newuser', document.getElementById("email").value, document.getElementById("password").value)
+    window.location.href = "http://localhost:3000/login"
     window.scrollTo(0, document.body.scrollHeight);
-    un.value = '';
-    pass.value = '';
+    document.getElementById("email").value = '';
+    document.getElementById("password").value = '';
+    document.getElementById("confirmPassword").value = '';
   }
-  newsignon.style.display = "none";
 });
